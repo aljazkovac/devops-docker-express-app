5b24878453037605532298a980e12fbc0d6f43d1
Aljaz Kovac <aljazkovac.kovacaljaz@gmail.com>
2025-04-06 21:23:46 +0200

    Test setup again.

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('<h1>Hello Wonderful Dockerized World!</h1>')
+  res.send('<h1>Hello Wonderfully Dockerized World!</h1>')
 })
 
 const PORT = 8080
